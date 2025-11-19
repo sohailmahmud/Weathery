b55b85cc87cbce6ddd2dfb7fecc5661dcf132f21
Sohail Mahmud <sohailmahmud@yahoo.com>
2025-11-19 10:35:55 +0600

    feat: Add WeatherModel and WeatherEntity classes for data representation

---
+++ lib/data/models/weather_model.dart
@@ -0,0 +1,110 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:weatherapp/domain/entities/weather_entity.dart';
+
+part 'weather_model.g.dart';
+
+@JsonSerializable()
+class WeatherModel {
+  @JsonKey(name: 'name')
+  final String cityName;
+
+  @JsonKey(name: 'main')
+  final MainData main;
+
+  @JsonKey(name: 'weather')
+  final List<WeatherData> weather;
+
+  @JsonKey(name: 'coord')
+  final CoordData coord;
+
+  @JsonKey(name: 'dt')
+  final int timestamp;
+
+  WeatherModel({
+    required this.cityName,
+    required this.main,
+    required this.weather,
+    required this.coord,
+    required this.timestamp,
+  });
+
+  factory WeatherModel.fromJson(Map<String, dynamic> json) =>
+      _$WeatherModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WeatherModelToJson(this);
+
+  WeatherEntity toEntity() {
+    return WeatherEntity(
+      cityName: cityName,
+      temperature: main.temp,
+      condition: weather.isNotEmpty ? weather[0].main : 'Unknown',
+      iconCode: weather.isNotEmpty ? weather[0].icon : '01d',
+      minTemp: main.tempMin,
+      maxTemp: main.tempMax,
+      latitude: coord.lat,
+      longitude: coord.lon,
+      timestamp: timestamp,
+    );
+  }
+}
+
+@JsonSerializable()
+class MainData {
+  final double temp;
+
+  @JsonKey(name: 'temp_min')
+  final double tempMin;
+
+  @JsonKey(name: 'temp_max')
+  final double tempMax;
+
+  final int humidity;
+  final int pressure;
+
+  MainData({
+    required this.temp,
+    required this.tempMin,
+    required this.tempMax,
+    required this.humidity,
+    required this.pressure,
+  });
+
+  factory MainData.fromJson(Map<String, dynamic> json) =>
+      _$MainDataFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MainDataToJson(this);
+}
+
+@JsonSerializable()
+class WeatherData {
+  final String main;
+  final String description;
+  final String icon;
+
+  WeatherData({
+    required this.main,
+    required this.description,
+    required this.icon,
+  });
+
+  factory WeatherData.fromJson(Map<String, dynamic> json) =>
+      _$WeatherDataFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WeatherDataToJson(this);
+}
+
+@JsonSerializable()
+class CoordData {
+  final double lon;
+  final double lat;
+
+  CoordData({
+    required this.lon,
+    required this.lat,
+  });
+
+  factory CoordData.fromJson(Map<String, dynamic> json) =>
+      _$CoordDataFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CoordDataToJson(this);
+}
---
+++ lib/domain/entities/weather_entity.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+
+class WeatherEntity extends Equatable {
+  final String cityName;
+  final double temperature;
+  final String condition;
+  final String iconCode;
+  final double minTemp;
+  final double maxTemp;
+  final double latitude;
+  final double longitude;
+  final int timestamp;
+
+  const WeatherEntity({
+    required this.cityName,
+    required this.temperature,
+    required this.condition,
+    required this.iconCode,
+    required this.minTemp,
+    required this.maxTemp,
+    required this.latitude,
+    required this.longitude,
+    required this.timestamp,
+  });
+
+  @override
+  List<Object?> get props => [
+        cityName,
+        temperature,
+        condition,
+        iconCode,
+        minTemp,
+        maxTemp,
+        latitude,
+        longitude,
+        timestamp,
+      ];
+}
